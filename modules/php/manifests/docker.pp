806e41a636763c407d2aa46a42e98a587a807575
argusyk <argusyk@gmail.com>
2023-12-09 16:46:23 +0200

    Docker php extension added! New image added!

--- modules/php/manifests/docker.pp
+++ modules/php/manifests/docker.pp
@@ -1,6 +1,9 @@
 class php::docker {
+    docker::image { 'php:8.0-fpmX':
+      docker_file => '/vagrant/etc/php/Dockerfile'
+    }
     docker::run { "php":
-      image   => "php:8.0-fpm",
+      image   => "php:8.0-fpmX",
       volumes => ['/vagrant/etc/php/php.ini:/usr/local/etc/php/php.ini',                   
                   '/vagrant/etc/www:/var/www/html'
                   ],
